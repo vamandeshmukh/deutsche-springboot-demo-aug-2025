c57367f8125574ec71b3b4cb01d1d203a1e69a57
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-07-30 12:36:51 +0530

    spring boot data jpa

--- src/main/java/com/deutsche/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/deutsche/demo/exception/GlobalExceptionHandler.java
@@ -4,15 +4,23 @@ import com.deutsche.demo.model.Employee;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(EmployeeNotFoundException.class)
     public ResponseEntity<Employee> handleEmployeeNotFoundException(EmployeeNotFoundException e) {
+//        return ResponseEntity
+//                .notFound()
+//                .header("message", e.getMessage())
+//                .body(null);
+
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .header("message", e.getMessage())
-                .body(null);  // ✔ Works fine
+                .body(null);
+
 
     }
 
